7cb50c2e7ee68dd7c888651b50aef84ed9ee08cb
eSsXiV <essxiv@eSsXiVs-MacBook-Pro.local>
2017-09-02 14:48:16 -0700

    favi

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var path = require('path');
 var bodyParser = require('body-parser');
 var db = require('./db/connection.js');
+var favicon = require('serve-favicon');
 var sendmail = require('sendmail')();
 
 var app = express();
